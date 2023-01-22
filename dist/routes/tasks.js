f2f4fdea26eeff7a2e8d9b116a74f2b403865476
Kamesas <semakaleksandr2014@gmail.com>
2023-01-22 18:28:19 +0200

    remove dist

--- dist/routes/tasks.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var express = require("express");
-var router = express.Router();
-router.get("/", function (req, res) {
-    res.send(["task one", "task two"]);
-});
-module.exports = router;
