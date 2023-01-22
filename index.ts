0268aa2e6f90e3decb5f277716958a45122ede3f
Kamesas <semakaleksandr2014@gmail.com>
2023-01-22 16:00:07 +0200

    init

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+var app: Express = express();
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello upd");
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
